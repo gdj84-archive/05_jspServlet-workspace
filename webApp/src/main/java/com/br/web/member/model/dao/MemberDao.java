9e58642b705f2b0fc1e45652e1c6ddd3f8754900
gdj84-archive <lecturekbr@naver.com>
2024-08-29 15:54:09 +0900

    update

---
+++ webApp/src/main/java/com/br/web/member/model/dao/MemberDao.java
@@ -0,0 +1,24 @@
+package com.br.web.member.model.dao;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class MemberDao {
+	
+	private Properties prop = new Properties();
+	
+	public MemberDao() {
+		
+		String filePath = MemberDao.class.getResource("/db/mappers/member-mapper.xml").getPath();
+		
+		try {
+			prop.loadFromXML(new FileInputStream(filePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+
+}
---
+++ webApp/src/main/java/com/br/web/member/model/service/MemberService.java
@@ -0,0 +1,5 @@
+package com.br.web.member.model.service;
+
+public class MemberService {
+
+}
