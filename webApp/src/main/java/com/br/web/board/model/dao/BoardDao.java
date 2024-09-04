851ee36ea517294bad6319108c8db45b8ab9346c
gdj84-archive <lecturekbr@naver.com>
2024-09-04 12:51:26 +0900

    update

--- webApp/src/main/java/com/br/web/board/model/dao/BoardDao.java
+++ webApp/src/main/java/com/br/web/board/model/dao/BoardDao.java
@@ -2,7 +2,13 @@ package com.br.web.board.model.dao;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Properties;
+
+import static com.br.web.common.template.JDBCTemplate.close;
 
 public class BoardDao {
 	
@@ -14,6 +20,32 @@ public class BoardDao {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public int selectBoardListCount(Connection conn) {
+		// select => ResultSet(게시글갯수, 숫자한개) => int
+		int listCount = 0;
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		String sql = prop.getProperty("selectBoardListCount");
+		
+		try {
+			pstmt = conn.prepareStatement(sql);
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				listCount = rset.getInt("count");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close(rset);
+			close(pstmt);
+		}
+		
+		return listCount;
+		
 	}
 	
 	
--- webApp/src/main/java/com/br/web/board/model/service/BoardService.java
+++ webApp/src/main/java/com/br/web/board/model/service/BoardService.java
@@ -1,10 +1,36 @@
 package com.br.web.board.model.service;
 
+import static com.br.web.common.template.JDBCTemplate.close;
+import static com.br.web.common.template.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+import java.util.List;
+
 import com.br.web.board.model.dao.BoardDao;
+import com.br.web.board.model.vo.Board;
+import com.br.web.common.model.vo.PageInfo;
 
 public class BoardService {
 	
 	private BoardDao bDao = new BoardDao();
+	
+	public int selectBoardListCount() {
+		Connection conn = getConnection();
+		int listCount = bDao.selectBoardListCount(conn);
+		close(conn);
+		return listCount;
+	}
+	
+	public List<Board> selectBoardList(PageInfo pi){
+		Connection conn = getConnection();
+		List<Board> list = bDao.selectBoardList(conn, pi);
+		close(conn);
+		return list;
+	}
+	
+	
+	
+	
 	
 	
 
