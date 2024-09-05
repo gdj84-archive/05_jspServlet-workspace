6f2af398a3835d6da7bfa59ce9fd2b6474ff8417
gdj84-archive <lecturekbr@naver.com>
2024-09-05 12:02:06 +0900

    update

--- webApp/src/main/java/com/br/web/common/utils/MyFileRenamePolicy.java
+++ webApp/src/main/java/com/br/web/common/utils/MyFileRenamePolicy.java
@@ -18,10 +18,13 @@ public class MyFileRenamePolicy implements FileRenamePolicy {
 		// 1. 파일업로드한 시간 (long currentTime)
 		long currentTime = System.currentTimeMillis(); // 1970/01/01 부터 현재시간까지 경과한 시간을 밀리세컨초로 반환
 		// 2. 랜덤숫자 5자리 (10000~99999) (int ranNum)
+		int ranNum = (int)(Math.random() * 90000 + 10000);
+		// 3. 원본파일의 확장자 (String ext)
+		String ext = originalFilename.substring(originalFilename.lastIndexOf("."));
 		
+		String filesystemName = currentTime + "_" + ranNum + ext;
 		
-		
-		return null;
+		return new File(originFile.getParent(), filesystemName);
 	}
 
 }
